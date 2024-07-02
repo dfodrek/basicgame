9718425f3a35b1ef4bf5198555e1488ff7d8ac18
Davor Fodrek <davor.fodrek@gmail.com>
2024-07-02 16:25:04 +0200

    World design

---
+++ Basic Game/Beach.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Beach extends World
+{
+
+    /**
+     * Constructor for objects of class Beach.
+     * 
+     */
+    public Beach()
+    {    
+        // Create a new world with 15x10 cells with a cell size of 60*60 pixels.
+        super(15, 10, 60); //cells in a row (width), height, dimension of 1 cell
+    }
+}
