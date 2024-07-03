a09bd6383f89d4134f3d38e57e749f97ddbe832f
Davor Fodrek <davor.fodrek@gmail.com>
2024-07-03 11:38:43 +0200

    poruke za kraj igre i ponovno pokretanje

--- Basic Game/Beach.java
+++ Basic Game/Beach.java
@@ -14,18 +14,12 @@ public class Beach extends World
     {    
         // Create a new world with 15x10 cells with a cell size of 60*60 pixels.
         super(15, 10, 60); //cells in a row (width), height, dimension of 1 cell
-        this.addObject(new Police("w","s","a","d"), 0, 0);
-        //this.addObject(new Police("up","down","left","right"), 5, 5);
-        this.addObject(
-            new Robber(),
-            Greenfoot.getRandomNumber(this.getWidth()),
-            Greenfoot.getRandomNumber(this.getHeight())
-        );
-        
-        this.counter=100;    //100 pozivanja metode act
+        this.prepareTheBeach();
     }
     
-    public void endTheGame(boolean win){  //parametar
+    public void endTheGame(boolean win){  //parametar win je opcija za poruku (win-loose)
+        
+        this.removeObjects(this.getObjects(Actor.class));  //brisanje svih objekata iz svijeta
         int x=this.getWidth()/2;    //pozicioniranje na sredinu Worlda: x,y koordinate
         int y=this.getHeight()/2;
         String message;
@@ -35,6 +29,9 @@ public class Beach extends World
             message="The robber escaped. You lost!";
         }
         this.showText(message, x, y);
+        Greenfoot.delay(40);   //pauza
+        this.showText(null, x, y);  //brisanje teksta sa svijeta
+        this.prepareTheBeach();  //ponovno pozivanje igre
         Greenfoot.stop();
     }
     
@@ -43,5 +40,17 @@ public class Beach extends World
         if (this.counter==0){   //kad se counter smanji na 0
             this.endTheGame(false);   //boolean uvjet je false - poruka za kraj igre se poziva
         }
+    }
+    
+    private void prepareTheBeach(){
+        this.addObject(new Police("w","s","a","d"), 0, 0);
+        //this.addObject(new Police("up","down","left","right"), 5, 5);
+        this.addObject(
+            new Robber(),
+            Greenfoot.getRandomNumber(this.getWidth()),
+            Greenfoot.getRandomNumber(this.getHeight())
+        );
+        
+        this.counter=500;    //100 pozivanja metode act  
     }
 }
